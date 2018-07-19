39085e579f5383df882f7aaa509a7ae1c4cf58be
Bartosz <szabon.bartosz@gmail.com>
2018-07-19 15:27:11 +0200

    Added new field to database

--- RazorPagesMovie/Models/SeedData.cs
+++ RazorPagesMovie/Models/SeedData.cs
@@ -25,7 +25,8 @@ namespace RazorPagesMovie.Models
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
-                        Price = 7.99M
+                        Price = 7.99M,
+                        Rating = "R"
                     },
 
                     new Movie
@@ -33,7 +34,8 @@ namespace RazorPagesMovie.Models
                         Title = "Ghostbusters",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
-                        Price = 8.99M
+                        Price = 8.99M,
+                        Rating = "W"
                     },
 
                     new Movie
@@ -41,7 +43,8 @@ namespace RazorPagesMovie.Models
                         Title = "Ghostbusters",
                         ReleaseDate = DateTime.Parse("1986-4-15"),
                         Genre = "Comedy",
-                        Price = 9.99M
+                        Price = 9.99M,
+                        Rating = "Z"
                     },
 
                     new Movie
@@ -49,7 +52,8 @@ namespace RazorPagesMovie.Models
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Westerm",
-                        Price = 3.99M
+                        Price = 3.99M,
+                        Rating = "X"
                     }
 
                     );
